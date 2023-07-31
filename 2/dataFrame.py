959f0fda256ccb01dd95cca80ffe99852935a75c
Sta Rog <satach@o2.pl>
2023-07-31 15:47:33 +0200

    1

--- 2/dataFrame.py
+++ 2/dataFrame.py
@@ -1,10 +1,35 @@
 import pandas as pd
+import math
+
+pd.set_option('display.max_rows', None)
 
 class DataFrame:
     def __init__(self, name):
         self.df = pd.read_csv(name)
 
+    def avg(self):
+        for Sex in set(self.df['Sex']):
+            for Pclass in set(self.df['Pclass']):
+                avg =(self.df[(self.df.Pclass == Pclass) & (self.df.Sex == Sex)])['Age'].mean()
+                print(f'The average age of {Sex} passangers with {Pclass} class ticket is {avg}')
+
+    def maturity(self):
+        def condition(age):
+            if age < 18:
+                return "Child"
+            elif age < 60:
+                return "Adult"
+            elif math.isnan(age):
+                return "Unknown"
+            else:
+                return "Oldie"
+        
+        self.df['Maturity'] = self.df["Age"].apply(lambda x: condition(x))
+        print(self.df.head(100))
 
 
-pp = DataFrame("Owners.csv")
-print(pp)
+titanic = DataFrame("2/titanic.csv")
+titanic.avg()
+titanic.maturity()
+
+
